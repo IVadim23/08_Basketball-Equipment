5d5bb62ca1ad731dcf742297e6a1215a53d7374b
IVadim23 <inazrski@gmail.com>
2024-08-04 23:52:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _08._Basketball_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tax per year:");
+            int taxPerYear = int.Parse(Console.ReadLine());
+            double shoesPrice = taxPerYear - taxPerYear * 40 / 100.0;
+            double outfitPrice = shoesPrice - shoesPrice * 20 / 100;
+            double ballPrice = outfitPrice / 4;
+            double accPrice = ballPrice / 5;
+            double sum = taxPerYear + shoesPrice + outfitPrice + ballPrice + accPrice;
+            Console.WriteLine("The final sum is:\n{0:f2}",sum);
+        }
+    }
+}
